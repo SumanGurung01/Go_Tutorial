a7134935dbc13fcf419f1bcdff289841484904b2
Suman Gurung <suman_201900250@smit.smu.edu.in>
2022-12-26 20:42:09 +0530

    Hello World in Go

--- file1.go
+++ file1.go
@@ -1,8 +1,17 @@
+/*
+	Date : Mon Dec 26 2022 20:34:47 GMT+0530 (India Standard Time)
+	Author : Suman Gurung
+	Description : Hello World Program in Go 
+*/
+
 package main
 
 import "fmt"
 
 func main(){
+	
+	// can use Print , Printf or Println
+	
 	fmt.Println("Hello")
 	fmt.Print("World")
 }
